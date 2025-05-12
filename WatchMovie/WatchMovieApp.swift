1574c884028e786c8ed0d898d290c4207d08ddfa
Jonasodiq <jonasniyazson@hotmail.com>
2025-05-12 23:18:10 +0200

    Initial Commit

---
+++ WatchMovie/WatchMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchMovieApp.swift
+//  WatchMovie
+//
+//  Created by Jonas Niyazson on 2025-05-12.
+//
+
+import SwiftUI
+
+@main
+struct WatchMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
